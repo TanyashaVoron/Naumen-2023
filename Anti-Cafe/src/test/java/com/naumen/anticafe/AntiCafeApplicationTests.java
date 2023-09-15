aee584867467fd6e2e5038c6fd6324df04750d74
Tatiana Voronina <82428036+TanyashaVoron@users.noreply.github.com>
2023-09-15 17:30:47 +0500

    Основа

---
+++ Anti-Cafe/src/test/java/com/naumen/anticafe/AntiCafeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.naumen.anticafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiCafeApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
