560ef3d6efc7995b5f6c30dd6b5ca754fd89bd75
TDF-SplitCore <timeryasovdf@gmail.com>
2023-10-04 23:18:44 +0500

    Фикс БД

--- Anti-Cafe/src/main/java/com/naumen/anticafe/repository/GuestCartRepository.java
+++ Anti-Cafe/src/main/java/com/naumen/anticafe/repository/GuestCartRepository.java
@@ -1,6 +1,7 @@
 package com.naumen.anticafe.repository;
 
+import com.naumen.anticafe.domain.GuestCart;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GuestCartRepository extends CrudRepository<GuestCartRepository, Long> {
+public interface GuestCartRepository extends CrudRepository<GuestCart, Long> {
 }
